a801ea2a3ef2fddeee2419b5c90a026af24af615
GAMMA Big Data Engineering <bcgikadigital@gmail.com>
2018-07-13 17:35:12 +0530

    Changes

--- scala-spark.sh
+++ scala-spark.sh
@@ -1,3 +1,4 @@
 wget 'http://www-us.apache.org/dist/spark/spark-2.3.0/spark-2.3.0-bin-hadoop2.7.tgz'
 tar -xvf spark-2.3.0-bin-hadoop2.7.tgz
 export PATH="$PATH:spark-2.3.0-bin-hadoop2.7/bin/"
+pip install -r requirements.txt --user
